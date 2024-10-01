941c5c001ef62e3a24699953e826f7e8a3a6d5ea
Daniel De La Asuncion <delaasunciondaniel@gmail.com>
2024-09-30 22:32:34 -0500

    some configs

--- src/fizzbuzz/fizzbuzz.service.spec.ts
+++ src/fizzbuzz/fizzbuzz.service.spec.ts
@@ -12,27 +12,27 @@ describe('FizzbuzzService', () => {
     service = module.get<FizzbuzzService>(FizzbuzzService);
   });
 
-  it('should be defined', () => {
+  it('Deberia ser definido', () => {
     expect(service).toBeDefined();
   });
 
-  it('should return Fizz when the number is multiple of 3', () => {
+  it('Deberia retornar Fizz cuando el numero sea multipo de 3', () => {
     expect(service.fizzbuzz(3)).toBe('Fizz');
   });
 
-  it('should return Buzz when the number is multiple of 5', () => {
+  it('Deberia retornar Buzz cuando el numero sea multiplo de 5', () => {
     expect(service.fizzbuzz(5)).toBe('Buzz');
   });
 
-  it('should return FizzBuzz when the number is multiple of 15', () => {
+  it('Deberia retornar FizzBuzz cuando el numero sea multiplo de 15', () => {
     expect(service.fizzbuzz(15)).toBe('FizzBuzz');
   });
 
-  it('should return the number when then number is neither multiple of 3, 5 nor 15', () => {
+  it('Deberia retornar el mismo numero cuando este no sea multiplo de 3, 5 o 15', () => {
     expect(service.fizzbuzz(2)).toBe(2);
   });
 
-  it('should return nothing when the number is not between 1 and 100', () => {
+  it('No deberia retornar nada cuando el numero no se encuentre entre 1 y 100', () => {
     expect(service.fizzbuzz(0)).toBe(undefined);
     expect(service.fizzbuzz(101)).toBe(undefined);
   });
